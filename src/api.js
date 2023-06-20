2e85a764069b2de6ab0d264580dc43e241ef199d
renatocfrancisco <renatocfrancisco@alunos.fho.edu.br>
2023-06-20 16:22:32 -0300

    refactor: :alien: refatorando consts de api
    a maioria será excluída depois

--- src/api.js
+++ src/api.js
@@ -1,36 +1,46 @@
 import axiosInstance from "./axios";
 
-export const login = (username, password) => {
-    return axiosInstance.post("/login", {
+export const login = async (username, password) => {
+    return await axiosInstance.post("/login", {
         username,
         password,
     });
 }
 
-export const getUsers = () => {
-    return axiosInstance.get("/user");
+export const setToken = (token) => {
+    localStorage.setItem("token", token);
 }
 
-export const getTasks = () => {
-    return axiosInstance.get("/task");
+export const setRefreshToken = (token) => {
+    localStorage.setItem("refreshToken", token);
 }
 
-export const updateTask = (id, data) => {
-    return axiosInstance.put(`/task/${id}`, data);
+export const logout = async () => {
+    await axiosInstance.get("/logout");
+    localStorage.removeItem("refreshToken");
+    localStorage.removeItem("token");
 }
 
-export const createTask = (data) => {
-    return axiosInstance.post("/task", data);
+export const getUsers = async () => {
+    return await axiosInstance.get("/user");
 }
 
-export const deleteTask = (id) => {
-    return axiosInstance.delete(`/task/${id}`);
+export const updateTask = async (id, data) => {
+    return await axiosInstance.put(`/task/${id}`, data);
 }
 
-export const updateUser = (id, data) => {
-    return axiosInstance.put(`/user/${id}`, data);
+export const deleteTask = async (id) => {
+    return await axiosInstance.delete(`/task/${id}`);
 }
 
-export const deleteUser = (id) => {
-    return axiosInstance.delete(`/user/${id}`);
+export const createUser = async (data) => {
+    return await axiosInstance.post("/user", data);
+}
+
+export const updateUser = async (id, data) => {
+    return await axiosInstance.put(`/user/${id}`, data);
+}
+
+export const deleteUser = async (id) => {
+    return await axiosInstance.delete(`/user/${id}`);
 }
