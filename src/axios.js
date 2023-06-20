4efe428a7871ce316350b4621da849a41f354c8b
renatocfrancisco <renatocfrancisco@alunos.fho.edu.br>
2023-06-20 16:21:42 -0300

    feat: :alien: interceptor axios para bearer token

--- src/axios.js
+++ src/axios.js
@@ -1,28 +1,22 @@
 import axios from 'axios'
 
-export const axiosInstance = axios.create({
+const axiosInstance = axios.create({
     baseURL: 'http://localhost:3000',
     timeout: 1000
 })
 
-const refreshAuthLogic = (
-    failedRequest,
-    config = {
-        method: "get",
-        url: "refresh",
-        headers: {
-            cookies: `${localStorage.getItem("refreshToken")}`,
-        },
-    }
-) =>
-    axiosInstance(config).then((response) => {
-        failedRequest.response.config.headers["Authorization"] =
-            "Bearer " + response.data.accessToken;
-        return Promise.resolve();
-    });
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
 
-createAuthRefreshInterceptor(axiosInstance, refreshAuthLogic, {
-    statusCodes: [403],
-});
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default axiosInstance
