0dab3d8ad2779da2cc36c702fe304101e032991f
renatocfrancisco <renatocfrancisco@alunos.fho.edu.br>
2023-06-22 15:02:49 -0300

    feat: :sparkles: retornar const para api.js, atualizar logout + format

--- src/api.js
+++ src/api.js
@@ -1,42 +1,55 @@
-import axiosInstance from "./axios";
+import axiosInstance from './axios'
 
 export const login = async (username, password) => {
-    return await axiosInstance.post("/login", {
-        username,
-        password,
-    });
+  return await axiosInstance.post('/login', {
+    username,
+    password
+  })
 }
 
 export const setToken = (token) => {
-    localStorage.setItem("token", token);
+  localStorage.setItem('token', token)
 }
 
 export const setRefreshToken = (token) => {
-    localStorage.setItem("refreshToken", token);
+  localStorage.setItem('refreshToken', token)
 }
 
 export const logout = async () => {
-    await axiosInstance.get("/logout");
-    localStorage.removeItem("refreshToken");
-    localStorage.removeItem("token");
+  await axiosInstance.get('/logout')
+  localStorage.removeItem('refreshToken')
+  localStorage.removeItem('token')
+}
+
+export const getTasks = async () => {
+  return await axiosInstance.get('/task')
+}
+
+export const createTask = async (data) => {
+  console.log(data)
+  return await axiosInstance.post('/task', data)
+}
+
+export const deleteTask = async (id) => {
+  return await axiosInstance.delete(`/task/${id}`)
 }
 
 export const getUsers = async () => {
-    return await axiosInstance.get("/user");
+  return await axiosInstance.get('/user')
 }
 
 export const updateTask = async (id, data) => {
-    return await axiosInstance.put(`/task/${id}`, data);
+  return await axiosInstance.put(`/task/${id}`, data)
 }
 
 export const createUser = async (data) => {
-    return await axiosInstance.post("/user", data);
+  return await axiosInstance.post('/user', data)
 }
 
 export const updateUser = async (id, data) => {
-    return await axiosInstance.put(`/user/${id}`, data);
+  return await axiosInstance.put(`/user/${id}`, data)
 }
 
 export const deleteUser = async (id) => {
-    return await axiosInstance.delete(`/user/${id}`);
+  return await axiosInstance.delete(`/user/${id}`)
 }
--- src/axios.js
+++ src/axios.js
@@ -1,22 +1,22 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000',
-    timeout: 1000
+  baseURL: 'http://localhost:3000',
+  timeout: 1000
 })
 
 axiosInstance.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers['Authorization'] = `Bearer ${token}`;
-        }
-
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
     }
-);
+
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
 
 export default axiosInstance
