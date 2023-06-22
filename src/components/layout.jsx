ba4ac15eda169d70524191465e97f3261b8bfeb4
renatocfrancisco <renatocfrancisco@alunos.fho.edu.br>
2023-06-22 15:04:17 -0300

    feat: :sparkles: barSpinner para loading, formatar layout.jsx

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,11 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
-    return (
+  return (
         <main className="App">
             <Outlet/>
-        </main>    
-    )
+        </main>
+  )
 }
 
 export default Layout
