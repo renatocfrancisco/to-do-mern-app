07a1029b400e92b105a55e1acce34cbc9f2a4791
renatocfrancisco <renatocfrancisco@alunos.fho.edu.br>
2023-07-04 12:52:06 -0300

    refactor: :fire: remover downArrow.jsx

--- src/assets/downArrow.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export function BiChevronDown(props) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16" {...props}><path fill="currentColor" fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"></path></svg>
-  )
-}
-export default BiChevronDown
