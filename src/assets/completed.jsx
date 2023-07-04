455b6e089a3b9fc4efde3337cfdd43105e1fa63d
renatocfrancisco <renatocfrancisco@alunos.fho.edu.br>
2023-07-04 15:17:50 -0300

    refactor: :bento: alterar svgs

--- src/assets/completed.jsx
+++ src/assets/completed.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export function BiCheckLg(props) {
+export function BiCheckLg (props) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 16 16" {...props}><path fill="currentColor" d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06a.733.733 0 0 1 1.047 0l3.052 3.093l5.4-6.425a.247.247 0 0 1 .02-.022Z"></path></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1.6em" height="1.6em" viewBox="0 0 16 16" {...props}><path fill="currentColor" d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06a.733.733 0 0 1 1.047 0l3.052 3.093l5.4-6.425a.247.247 0 0 1 .02-.022Z"></path></svg>
   )
 }
 export default BiCheckLg
